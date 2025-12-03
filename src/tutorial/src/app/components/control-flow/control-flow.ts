1895c1e0766fbcf685ecfb31ea4a0bf834681404
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-12-03 01:06:29 -0300

    main: fix (tutorial) - Tweak control-flow component template and logic
    - Refined the tutorial app's control-flow example by adjusting the component template and updating component logic to improve rendering and interaction.

--- src/tutorial/src/app/components/control-flow/control-flow.ts
+++ src/tutorial/src/app/components/control-flow/control-flow.ts
@@ -13,6 +13,33 @@ export class ControlFlow {
 
   isSuccessDivVisiable: WritableSignal<boolean> = signal(false);
 
+  offerList: string[] = [
+    '15% de desconto no Pix',
+    '30% de desconto no cartão Visa',
+    '20% de desconto em compras acima de R$ 100',
+    'Frete grátis para pagamentos no PayPal',
+  ];
+
+  productCategoryList: string[] = [
+    'Ferramentas Manuais',
+    'Livros de Ficção',
+    'Roupas Casuais',
+    'Utensílios de Cozinha',
+    'Brinquedos Educativos',
+    'Produtos de Jardinagem',
+  ];
+
+  employeeList = [
+    { name: 'Ana Beatriz Souza', city: 'São Paulo', isActive: false },
+    { name: 'Carlos Henrique Lima', city: 'Rio de Janeiro', isActive: false },
+    { name: 'Giovanna Martins Duarte', city: 'Curitiba', isActive: false },
+    { name: 'Rafael dos Anjos Moreira', city: 'Belo Horizonte', isActive: true },
+    { name: 'Luiza Ferreira Campos', city: 'Salvador', isActive: false },
+    { name: 'Miguel Andrade Pires', city: 'Fortaleza', isActive: false },
+    { name: 'Bruna Oliveira Costa', city: 'Recife', isActive: false },
+    { name: 'Eduardo Ramos Figueiredo', city: 'Porto Alegre', isActive: true },
+  ];
+
   toggleDivVisibility() {
     this.isSuccessDivVisiable.set(!this.isSuccessDivVisiable());
   }
