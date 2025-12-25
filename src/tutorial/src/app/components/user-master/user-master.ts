e9d95133a88827879184b73d4ed7a9587f619c29
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-12-25 16:52:16 -0300

    main: style (tutorial) - Removed unused JsonPipe import and related code

--- src/tutorial/src/app/components/user-master/user-master.ts
+++ src/tutorial/src/app/components/user-master/user-master.ts
@@ -1,15 +1,14 @@
-import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'user-master',
-  imports: [FormsModule, JsonPipe],
+  imports: [FormsModule],
   templateUrl: './user-master.html',
   styleUrl: './user-master.css',
 })
 export class UserMaster {
-  
+
   userObj = {
     id: 0,
     name: '',
