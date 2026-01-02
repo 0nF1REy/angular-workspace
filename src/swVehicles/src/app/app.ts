1c2352da7c9049454cd9cb5b8bda7bc3321d93b4
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2026-01-02 14:38:20 -0300

    main: style (swVehicles) - Updated page title to Portuguese localization

--- src/swVehicles/src/app/app.ts
+++ src/swVehicles/src/app/app.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'swv-root',
-    imports: [RouterOutlet, RouterLink, RouterLinkActive],
-    templateUrl: './app.html',
-    styleUrls: ['./app.css']
+  selector: 'swv-root',
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  templateUrl: './app.html',
+  styleUrls: ['./app.css'],
 })
 export class App {
-  pageTitle = 'Star Wars Vehicle Sales';
+  pageTitle = 'Venda de Veículos - Star Wars';
 }
