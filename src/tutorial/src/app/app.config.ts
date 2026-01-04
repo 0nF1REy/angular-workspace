734d59251c15257d58b3bc228df47dea7184f7b1
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2026-01-04 01:28:55 -0300

    main: chore (tutorial) - Updated package dependencies and application configuration
    - Updated the package dependencies and refined the application configuration for improved routing and state management.

--- src/tutorial/src/app/app.config.ts
+++ src/tutorial/src/app/app.config.ts
@@ -1,11 +1,17 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideStore(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+    provideEffects(),
+  ],
 };
