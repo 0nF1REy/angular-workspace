a755c960fd92c40d10bfc9fbfa1df5f63c2b46f5
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2026-01-05 18:06:07 -0300

    main: feat (tutorial/config) - Implemented custom title strategy and updated route titles for better navigation context

--- src/tutorial/src/app/app.config.ts
+++ src/tutorial/src/app/app.config.ts
@@ -1,22 +1,44 @@
 import {
   ApplicationConfig,
+  inject,
   isDevMode,
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
+  Injectable,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  provideRouter,
+  RouterStateSnapshot,
+  TitleStrategy,
+  withComponentInputBinding,
+} from '@angular/router';
 import { provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { provideEffects } from '@ngrx/effects';
+import { Title } from '@angular/platform-browser';
 
 import { routes } from './app.routes';
 import { tReducers } from './core/store';
+
+@Injectable({ providedIn: 'root' })
+class CustomTitleStrategy extends TitleStrategy {
+  private readonly titleService = inject(Title);
+
+  override updateTitle(routerState: RouterStateSnapshot): void {
+    const title = this.buildTitle(routerState);
+    if (title !== undefined) {
+      this.titleService.setTitle(`Tutorial - ${title}`);
+    } else {
+      this.titleService.setTitle('Tutorial Lab');
+    }
+  }
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(routes, withComponentInputBinding()),
     provideStore(tReducers),
     provideEffects(),
     provideStoreDevtools({
@@ -24,5 +46,6 @@ export const appConfig: ApplicationConfig = {
       logOnly: !isDevMode(),
       trace: true,
     }),
+    { provide: TitleStrategy, useClass: CustomTitleStrategy },
   ],
 };
--- src/tutorial/src/app/app.routes.ts
+++ src/tutorial/src/app/app.routes.ts
@@ -8,13 +8,12 @@ export const routes: Routes = [
     redirectTo: 'login',
     pathMatch: 'full',
   },
-
   {
     path: 'login',
+    title: 'Login',
     canActivate: [guestGuard],
     loadComponent: () => import('./pages/auth/login/login').then((m) => m.Login),
   },
-
   {
     path: '',
     loadComponent: () => import('./core/layout/layout').then((m) => m.Layout),
@@ -22,48 +21,49 @@ export const routes: Routes = [
     children: [
       {
         path: 'variables',
+        title: 'Variável',
         loadComponent: () => import('./pages/variables/variables').then((m) => m.Variables),
       },
-
       {
         path: 'databinding',
+        title: 'Data Binding',
         loadComponent: () => import('./pages/data-binding/data-binding').then((m) => m.DataBinding),
       },
-
       {
         path: 'signal',
+        title: 'Signal',
         loadComponent: () => import('./pages/signal/signal').then((m) => m.SignalComponent),
       },
-
       {
         path: 'control-flow',
+        title: 'Fluxo de Controle',
         loadComponent: () => import('./pages/control-flow/control-flow').then((m) => m.ControlFlow),
       },
-
       {
         path: 'dynamic-css-class',
+        title: 'Estilização Dinâmica',
         loadComponent: () =>
           import('./pages/dynamic-css-class/dynamic-css-class').then((m) => m.DynamicCssClass),
       },
-
       {
         path: 'users',
+        title: 'Formulário',
         loadComponent: () => import('./pages/user-master/user-master').then((m) => m.UserMaster),
       },
-
       {
         path: 'reactive-users',
+        title: 'Formulário Reativo',
         loadComponent: () =>
           import('./pages/reactive-user/reactive-user').then((m) => m.ReactiveUser),
       },
-
       {
         path: 'batch',
+        title: 'Lotes',
         loadComponent: () => import('./pages/batch-master/batch-master').then((m) => m.BatchMaster),
       },
-
       {
         path: 'competition',
+        title: 'Competição',
         loadComponent: () =>
           import('./pages/project-competition/project-competition').then(
             (m) => m.ProjectCompetition
@@ -71,23 +71,26 @@ export const routes: Routes = [
       },
       {
         path: 'signal-form',
+        title: 'Signal Form',
         loadComponent: () =>
           import('./pages/signal-form-ex/signal-form-ex').then((m) => m.SignalFormEx),
       },
       {
         path: 'life-cycle',
+        title: 'Ciclo de Vida',
         loadComponent: () =>
           import('./pages/life-cycle-ex/life-cycle-ex').then((m) => m.LifeCycleEx),
       },
       {
         path: 'counter',
+        title: 'Gestão de Estado (NgRx)',
         loadComponent: () => import('./pages/counter/counter').then((m) => m.Counter),
       },
     ],
   },
-
   {
     path: '**',
     loadComponent: () => import('./pages/not-found/not-found').then((m) => m.NotFound),
+    title: 'Erro 404',
   },
 ];
