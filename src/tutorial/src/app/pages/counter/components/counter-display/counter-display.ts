96587ebfdae191573420472e2d07357d36d7f41a
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2026-01-05 01:43:18 -0300

    main: feat (tutorial/counter) - Added direction-based animations to counter display component
    - Implemented change detection logic using Angular signals and computed properties to track counter value direction.
    - Added bump-up and bump-down animations that automatically apply based on whether the value increased or decreased, with timeout cleanup to remove animation classes after 300ms.

--- src/tutorial/src/app/pages/counter/components/counter-display/counter-display.ts
+++ src/tutorial/src/app/pages/counter/components/counter-display/counter-display.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'counter-display',
@@ -7,5 +7,28 @@ import { Component, Input } from '@angular/core';
   styleUrl: './counter-display.css',
 })
 export class CounterDisplay {
-  @Input({ required: true }) value: number = 0;
+  @Input({ required: true }) set value(val: number) {
+    this.updateDirection(val);
+    this._value.set(val);
+  }
+
+  private _value = signal(0);
+
+  protected displayValue = computed(() => this._value());
+
+  protected animationClass = signal('');
+
+  private lastValue = 0;
+
+  private updateDirection(newValue: number) {
+    if (newValue > this.lastValue) {
+      this.animationClass.set('bump-up');
+    } else if (newValue < this.lastValue) {
+      this.animationClass.set('bump-down');
+    }
+
+    this.lastValue = newValue;
+
+    setTimeout(() => this.animationClass.set(''), 300);
+  }
 }
