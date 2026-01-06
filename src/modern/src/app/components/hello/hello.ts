b6f4138e3a13c628051fe21f83126235d8cd9b23
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2026-01-06 14:14:56 -0300

    main: feat (modern/hello) - Added hello component scaffold
    - Introduced a hello component with template, styles, and tests.
    - Integrated it into the modern app shell and align the build setup for the new component.

---
+++ src/modern/src/app/components/hello/hello.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'm-hello',
+  imports: [],
+  templateUrl: './hello.html',
+  styleUrl: './hello.scss',
+})
+export class Hello {}
