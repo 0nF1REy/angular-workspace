f7d047401a54e7f53cf8c1714c83eb66915389e5
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2026-01-11 18:57:25 -0300

    main: feat (tut/app) - Updated app structure with new name variables and dynamic update functions; enhanced button styles

--- src/tut/src/app/app.ts
+++ src/tut/src/app/app.ts
@@ -11,11 +11,73 @@ import { DatePipe } from '@angular/common';
 export class App {
   protected readonly title = signal('tut');
 
-  name = 'Alan Ryan da Silva Domingues';
+  nameOne = 'Alan Ryan da Silva Domingues';
+  nameTwo = signal('Alan Ryan da Silva Domingues');
   age = 21;
   currentDate = signal(new Date());
 
-  updateName() {
-    this.name = 'Lorem ipsum';
+  updateNameOne() {
+    this.nameOne = 'A. R. D. S. D';
+  }
+
+  updateNameTwo() {
+    // Geração de texto aleatório de 50 a 100 palavras
+    const hugeText = this.generateLorem(80);
+    this.nameTwo.set(hugeText);
+  }
+
+  private generateLorem(wordCount: number): string {
+    const words = [
+      'lorem',
+      'ipsum',
+      'dolor',
+      'sit',
+      'amet',
+      'consectetur',
+      'adipiscing',
+      'elit',
+      'sed',
+      'do',
+      'eiusmod',
+      'tempor',
+      'incididunt',
+      'ut',
+      'labore',
+      'et',
+      'dolore',
+      'magna',
+      'aliqua',
+      'enim',
+      'ad',
+      'minim',
+      'veniam',
+      'quis',
+      'nostrud',
+      'exercitation',
+      'ullamco',
+      'laboris',
+      'nisi',
+      'ut',
+      'aliquip',
+      'ex',
+      'ea',
+      'commodo',
+      'consequat',
+      'duis',
+      'aute',
+      'irure',
+      'dolor',
+      'in',
+      'reprehenderit',
+    ];
+
+    let result = '';
+    for (let i = 0; i < wordCount; i++) {
+      const randomIndex = Math.floor(Math.random() * words.length);
+      result += words[randomIndex] + ' ';
+    }
+
+    // Capitalização da primeira letra e coloca um ponto final
+    return result.trim().charAt(0).toUpperCase() + result.trim().slice(1) + '.';
   }
 }
