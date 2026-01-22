7511db59f5eab265fae2ea739c7ee54bdd5055bc
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2026-01-22 00:27:21 -0300

    main: chore (signal-forms/ex-01) - Moved project files into ex-01 subfolder
    - Relocated all core files, configuration, and assets of the signal-forms project into the ex-01 subdirectory for improved structure and modularity.
    - Removed obsolete files from the root and ensured all references and resources are now under ex-01.

--- src/signal-forms/ex-01/src/app/app.ts
+++ src/signal-forms/ex-01/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('angular-21');
+  protected readonly title = signal('ex-01');
 }
